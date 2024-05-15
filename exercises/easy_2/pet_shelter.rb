c6e49f60de2d9b276042ac0a51685184a4e3e5cd
Alyssa Easter <100172781+alyssa43@users.noreply.github.com>
2024-05-15 13:54:36 -0700

    Completed all of Easy 2 exercises

--- exercises/easy_2/pet_shelter.rb
+++ exercises/easy_2/pet_shelter.rb
@@ -1,29 +1,73 @@
+EMPTY_LINE = ' '
+
 class Pet
+  attr_reader :animal, :name
+
   def initialize(animal, name)
     @animal = animal
     @name = name
   end
+
+  def to_s
+    puts "a #{animal} named #{name}"
+  end
 end
 
 class Owner
+  attr_reader :name, :pets
+
   def initialize(name)
     @name = name
+    @pets = []
   end
 
-  def adopted_pets
+  def add_pet(animal, name)
+    pets << [animal, name]
+  end
 
+  def number_of_pets
+    pets.count
+  end
 end
 
 class Shelter
+  attr_reader :adopted, :adoptable
+
   def initialize
-    @adopted = Hash.new 
+    @adopted = {}
+    @adoptable = []
+  end
+
+  def existing_client?(client_name)
+    adopted.key?(client_name.name)
   end
 
   def adopt(owner_name, pet_name)
-    
+    owner_name.add_pet(pet_name.animal, pet_name.name)
+    adopted[owner_name.name] = [] unless existing_client?(owner_name)
+    adopted[owner_name.name] << [pet_name.animal, pet_name.name]
   end
-  
+
+  def add_adoptable_pet(pet)
+    adoptable << [pet.animal, pet.name]
+  end
+
   def print_adoptions
+    adopted.each do |client, pets|
+      puts "#{client} has adopted the following pets:"
+      pets.each { |pet_array| puts pet_array[1] }
+      puts EMPTY_LINE
+    end
+  end
+
+  def print_adoptable_pets
+    puts 'The Animal Shelter has the following unadopted pets:'
+    adoptable.each { |pet_array| puts pet_array[1] }
+    puts EMPTY_LINE
+  end
+
+  def number_of_pets
+    adoptable.count
   end
 end
 
@@ -34,6 +78,13 @@ kennedy      = Pet.new('dog', 'Kennedy')
 sweetie      = Pet.new('parakeet', 'Sweetie Pie')
 molly        = Pet.new('dog', 'Molly')
 chester      = Pet.new('fish', 'Chester')
+asta         = Pet.new('dog', 'Asta')
+laddie       = Pet.new('dog', 'Laddie')
+fluffy       = Pet.new('cat', 'Fluffy')
+kat          = Pet.new('cat', 'Kat')
+ben          = Pet.new('cat', 'Ben')
+chatterbox   = Pet.new('parakeet', 'Chatterbox')
+bluebell     = Pet.new('parakeet', 'Bluebell')
 
 phanson = Owner.new('P Hanson')
 bholmes = Owner.new('B Holmes')
@@ -46,16 +97,18 @@ shelter.adopt(bholmes, kennedy)
 shelter.adopt(bholmes, sweetie)
 shelter.adopt(bholmes, molly)
 shelter.adopt(bholmes, chester)
-shelter.print_adoptions
-# P Hanson has adopted the following pets:
-# a cat named Butterscotch
-# a cat named Pudding
-# a bearded dragon named Darwin
 
-# B Holmes has adopted the following pets:
-# a dog named Molly
-# a parakeet named Sweetie Pie
-# a dog named Kennedy
-# a fish named Chester
-puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets." # => P Hanson has 3 adopted pets.
-puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets." # => B Holmes has 4 adopted pets.
+shelter.add_adoptable_pet(asta)
+shelter.add_adoptable_pet(laddie)
+shelter.add_adoptable_pet(fluffy)
+shelter.add_adoptable_pet(kat)
+shelter.add_adoptable_pet(ben)
+shelter.add_adoptable_pet(chatterbox)
+shelter.add_adoptable_pet(bluebell)
+
+shelter.print_adoptable_pets
+shelter.print_adoptions
+
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+puts "The Animal shelter has #{shelter.number_of_pets} unadopted pets."
