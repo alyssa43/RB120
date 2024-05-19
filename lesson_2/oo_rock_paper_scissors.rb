b902f2e33d64f0ebe84bafeba7d7ed9dd29921f5
Alyssa Easter <AlyssaStarley@gmail.com>
2024-05-19 16:36:37 -0700

    Completed refactoring

--- lesson_2/oo_rock_paper_scissors.rb
+++ lesson_2/oo_rock_paper_scissors.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Move
-  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+  MOVES = %w(rock paper scissors lizard spock)
 
   def initialize(value)
     @value = value
@@ -28,29 +28,19 @@ class Move
   end
 
   def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?) ||
-      (rock? && other_move.lizard?) ||
-      (lizard? && other_move.spock?) ||
-      (spock? && other_move.scissors?) ||
-      (scissors? && other_move.lizard?) ||
-      (lizard? && other_move.paper?) ||
-      (paper? && other_move.spock?) ||
-      (spock? && other_move.rock?)
+    (rock? && (other_move.scissors? || other_move.lizard?)) ||
+      (paper? && (other_move.rock? || other_move.spock?)) ||
+      (scissors? && (other_move.paper? || other_move.lizard?)) ||
+      (spock? && (other_move.scissors? || other_move.rock?)) ||
+      (lizard? && (other_move.spock? || other_move.paper?))
   end
 
   def <(other_move)
-    (rock? && other_move.paper?) ||
-      (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?) ||
-      (lizard? && other_move.rock?) ||
-      (spock? && other_move.lizard?) ||
-      (scissors? && other_move.spock?) ||
-      (lizard? && other_move.scissors?) ||
-      (paper? && other_move.lizard?) ||
-      (spock? && other_move.paper?) ||
-      (rock? && other_move.spock?)
+    ((rock? && other_move.paper?) || other_move.spock?) ||
+      ((paper? && other_move.scissors?) || other_move.lizard?) ||
+      ((scissors? && other_move.rock?) || other_move.spock?) ||
+      ((spock? && other_move.lizard?) || other_move.paper?) ||
+      ((lizard? && other_move.rock?) || other_move.scissors?)
   end
 
   def to_s
@@ -85,7 +75,7 @@ class Human < Player
     loop do
       puts '=> Please choose rock, paper, scissors, lizard, or spock:'
       choice = gets.chomp.downcase
-      break if Move::VALUES.include?(choice)
+      break if Move::MOVES.include?(choice)
       puts "Sorry, invalid choice."
     end
     self.move = Move.new(choice)
@@ -98,11 +88,11 @@ class Computer < Player
   end
 
   def choose
-    self.move = Move.new(Move::VALUES.sample)
+    self.move = Move.new(Move::MOVES.sample)
   end
 end
 
-class GameFormat
+module Displayable
   DISPLAY_SIZE = 80
 
   def center_text(text)
@@ -116,7 +106,7 @@ class GameFormat
   def display_border(text)
     width = text.length + 4
     horizontal = "-" * width
-    vertical = "|" + (' ' * (width - 2)) + "|"
+    vertical = "|#{(' ' * (width - 2))}|"
 
     center_text(horizontal)
     center_text(vertical)
@@ -124,34 +114,15 @@ class GameFormat
     center_text(vertical)
     center_text(horizontal)
   end
-    
-  
+
   def display_empty_line
     puts ''
-  end
-end
-
-# Game Orchestration Engine
-class RPSGame < GameFormat
-  WINNING_SCORE = 10
-
-  attr_accessor :human, :computer, :ties, :winner, :grand_winner
-
-  def initialize
-    @human = Human.new
-    @computer = Computer.new
-    @ties = 0
   end
 
   def display_welcome_message
     system 'clear'
-    center_text("---> #{human.name}, welcome to Rock, Paper, Scissors, Lizard, Spock! <---")
+    center_text("---> Welcome to Rock, Paper, Scissors, Lizard, Spock! <---")
     center_text("The first to reach 10 points is the winner!")
-    display_empty_line
-  end
-
-  def display_goodbye_message
-    center_text("---> Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Good bye! <---")
     display_empty_line
   end
 
@@ -159,18 +130,18 @@ class RPSGame < GameFormat
     center_text("SCOREBOARD")
     display_border("---> #{human.name}'s score: #{human.score} | #{computer.name}'s score: #{computer.score} | ties: #{@ties} <---")
   end
-    
+
   def display_moves
+    display_empty_line
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
+    display_empty_line
   end
 
   def display_winner
     system 'clear'
     display_score_board
-    display_empty_line
     display_moves
-    display_empty_line
 
     if winner.nil?
       puts "It's a tie!"
@@ -182,6 +153,8 @@ class RPSGame < GameFormat
   end
 
   def display_final_score
+    display_border("EXITING NOW...")
+    sleep 2
     system 'clear'
     center_text("GAME OVER! THE FINAL SCORE IS:")
     display_empty_line
@@ -190,25 +163,49 @@ class RPSGame < GameFormat
     center_text("Congratulations, #{grand_winner}, you are the winner!") if grand_winner
   end
 
-  def determine_winner
-    if human.move > computer.move
-      @winner = human.name
-    elsif human.move < computer.move
-      @winner = computer.name
-    else
-      @winner = nil
-    end
+  def display_goodbye_message
+    display_final_score
+    center_text("---> Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Good bye! <---")
+    display_empty_line
+  end
+end
+
+# Game Orchestration Engine
+class RPSGame
+  include Displayable
+  attr_accessor :human, :computer, :ties, :winner, :grand_winner
+
+  WINNING_SCORE = 10
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+    @ties = 0
   end
 
-  def update_score
-    if winner == human.name
-      human.score += 1
-    elsif winner == computer.name
-      computer.score += 1
-    else
-      @ties += 1
-    end
+  def determine_winner
+    @winner = if human.move > computer.move
+                human.score += 1
+                human.name
+              elsif human.move < computer.move
+                computer.score += 1
+                computer.name
+              else
+                @ties += 1
+              end
   end
+
+  # def update_score
+  #   if winner == human.name
+  #     human.score += 1
+  #     @winner = human.name
+  #   elsif winner == computer.name
+  #     computer.score += 1
+  #     @winner = computer.name
+  #   else
+  #     @ties += 1
+  #   end
+  # end
 
   def grand_winner?
     if human.score == WINNING_SCORE
@@ -223,23 +220,23 @@ class RPSGame < GameFormat
   end
 
   def next_round?
-    puts "=> Push any button to continue to the next round, or 'q' to quit."
+    prompt("Push any button to continue to the next round, or 'q' to quit.")
     answer = gets.chomp.downcase
 
     quit = quit_game? if answer.start_with?('q')
 
     return false if quit
-    return true
+    true
   end
 
   def quit_game?
     answer = ''
 
     loop do
-      puts "=> Are you sure you want to quit? (yes/no)"
+      prompt("Are you sure you want to quit? (yes/no)")
       answer = gets.chomp.downcase
       break if answer.start_with?('y') || answer.start_with?('n')
-      puts "Invalid input, please enter either 'yes' or 'no'"
+      prompt("Invalid input, please enter either 'yes' or 'no'")
     end
 
     answer.start_with?('y')
@@ -251,12 +248,10 @@ class RPSGame < GameFormat
       human.choose
       computer.choose
       determine_winner
-      update_score
       display_winner
       break if grand_winner?
       break unless next_round?
     end
-    display_final_score
     display_goodbye_message
   end
 end
